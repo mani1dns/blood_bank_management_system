97a730898ce7108d4e7a6e48748a8040ba0529f6
Mani A <mani.msd1357@gmail.com>
2023-08-27 09:41:41 +0530

    donor registration,search, admin, contact pages are created and linked with base.html

--- blood/home/urls.py
+++ blood/home/urls.py
@@ -4,4 +4,8 @@ from home import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
+    path('donor/', views.donor, name="donor"),
+    path('search/', views.search, name="search"),
+    path('admin/', views.admin, name="admin"),
+    path('contact/', views.contact, name="contact"),
 ]
--- blood/home/views.py
+++ blood/home/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+def donor(request):
+    return render(request, "donor.html")
+
+def search(request):
+    return render(request, "search.html")
+
+def admin(request):
+    return render(request, "admin.html")
+
+def contact(request):
+    return render(request, "contact.html")
